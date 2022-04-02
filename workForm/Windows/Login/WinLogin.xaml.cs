60ae9e9c16c04249687956f4674de0e152fd1e78
MandelOwO <87827886+MandelOwO@users.noreply.github.com>
2022-04-02 13:53:26 +0200

    Login window - layout

---
+++ workForm/Windows/Login/WinLogin.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace workForm.Windows.Login
+{
+    /// <summary>
+    /// Interaction logic for WinLogin.xaml
+    /// </summary>
+    public partial class WinLogin : Window
+    {
+        public WinLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new PgLogin();
+        }
+    }
+}
